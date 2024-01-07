e7d95cc27b8f0887307b51706b71aae0653e96e9
Gensoul <gensoul030929@outlook.com>
2024-01-07 12:15:05 +0800

    Code adjustments.

--- src/observer/sql/operator/predicate_operator.cpp
+++ src/observer/sql/operator/predicate_operator.cpp
@@ -87,6 +87,7 @@ bool PredicateOperator::do_predicate(Tuple &tuple)
     return true;
   }
 
+  // the relation between two filter_units is AND
   for (const FilterUnit *filter_unit : filter_stmt_->filter_units()) {
     Expression *left_expr = filter_unit->left();
     Expression *right_expr = filter_unit->right();
@@ -115,26 +116,7 @@ bool PredicateOperator::do_predicate(Tuple &tuple)
       continue;
     }
 
-    // for compare.
-    
-    float *tmp_left_float = nullptr;
-    float *tmp_right_float = nullptr;
-    DEFER([&]() {
-      if (tmp_left_float)
-        delete tmp_left_float;
-      if (tmp_right_float)
-        delete tmp_right_float;
-    });
-    if (left_type != right_type) {
-      tmp_left_float = (float *)cast_to[left_type][FLOATS]((void *)left_cell.data());
-      std::cout << *tmp_left_float << std::endl;
-      left_cell.set_data((char *)tmp_left_float);
-      left_cell.set_type(FLOATS);
-      tmp_right_float = (float *)cast_to[right_type][FLOATS]((void *)right_cell.data());
-      std::cout << *tmp_right_float << std::endl;
-      right_cell.set_data((char *)tmp_right_float);
-      right_cell.set_type(FLOATS);
-    }
+    // for compare: > >= < <= == != <>
     
     const int compare = left_cell.compare(right_cell);
     bool filter_result = false;
